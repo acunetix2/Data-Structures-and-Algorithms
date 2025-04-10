e174f9da03d4ec7c730bd67a2ea2a226ad60d911
Iddy Chesire <iddychesire098@gmail.com>
2025-04-10 09:20:56 +0300

    Add files via upload

---
+++ Queuestack.c
@@ -0,0 +1,74 @@
+#include<stdio.h>
+#include<stdbool.h>
+#define SIZE 10
+
+int stack[SIZE];
+int rear = -1;
+int front = -1;
+
+void push(int value)
+{
+    if(isFull())
+    {
+        printf("The Queue is Full!");
+    }
+    else if(front == -1 && front == -1)
+    {
+        front++;
+        stack[front] = value;
+        rear++;
+    }
+    else
+    {
+        front++;
+        stack[front] = value;
+    }
+}
+bool isEmpty()
+{
+    if(front == -1 && rear == -1)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+bool isFull()
+{
+    if(rear + 1 == SIZE)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+void pop()
+{
+    if(isEmpty())
+    {
+        printf("The queue is Empty!");
+    }
+    else if(front == 0 && rear == 0)
+    {
+        printf("\n%d",stack[front]);
+        front--;
+        rear--;
+    }
+    else
+    {
+        printf("\n%d",stack[front]);
+        front--;
+    }
+}
+void enqueue(int data)
+{
+    push(data);
+}
+void dequeue()
+{
+    pop();
+}
